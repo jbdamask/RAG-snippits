af9bbf971ece2b908852d161c9cba03e92b51b5d
@jbdamaskfsp <jdamask@flagshippioneering.com>
2023-12-03 09:37:58 -0500

    Added basic chainlit app

---
+++ chainlit/chainlit.md
@@ -0,0 +1,4 @@
+# Welcome to CoolCo's Lab Protocol ChatBot! 🚀🤖
+
+Hi there! You can use this chatbot to look up lab protocols from our library
+
--- loading-data/s3-pdf-to-pinecone.py
+++ loading-data/s3-pdf-to-pinecone.py
@@ -4,14 +4,16 @@ import boto3
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import PyPDFLoader
+from langchain.docstore.document import Document
 import pinecone
 from langchain.vectorstores import Pinecone
 from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv()) # read local .env file
+_ = load_dotenv(find_dotenv('../.env')) # read local .env file
 
 AWS_ACCESS_KEY = var = os.getenv('AWS_ACCESS_KEY')
 AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
 AWS_REGION = os.getenv('AWS_REGION')
+BUCKET_NAME = os.getenv('BUCKET_NAME')
 OPENAI_API_KEY=os.getenv('OPENAI_API_KEY')
 PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 PINECONE_ENVIRONMENT = os.getenv('PINECONE_ENVIRONMENT')
@@ -22,12 +24,13 @@ pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
 embeddings = OpenAIEmbeddings(api_key=OPENAI_API_KEY)
 vector_store = Pinecone.from_existing_index(PINECONE_INDEX, embeddings)
 
-# initialize boto3 client   
 s3 = boto3.client('s3', 
                   aws_access_key_id=AWS_ACCESS_KEY, 
                   aws_secret_access_key=AWS_SECRET_KEY,
                   region_name=AWS_REGION)
 
+
+local_directory = 'tmp_pdfs'
 
 """ 
 Recursively search through a directory tree, filtering for pdf files.
@@ -35,20 +38,20 @@ For each file, create a LangChain PyPDFLoader, create a list of documents
 using load_and_split with a RecursiveCharacterTextSplitter, and load the
 docs into Pinecone
 """
-def load_pdfs(bucket_name, local_directory):
+def load_pdfs():
 
     """
     List and download all PDF files from the bucket
     """
     def list_files():
         paginator = s3.get_paginator('list_objects_v2')
-        for page in paginator.paginate(Bucket=bucket_name):
+        for page in paginator.paginate(Bucket=BUCKET_NAME):
             for obj in page.get('Contents', []):
                 file_name = obj['Key']
                 if file_name.endswith('.pdf'):
                     local_file_path = os.path.join(local_directory, file_name)
                     os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
-                    s3.download_file(bucket_name, file_name, local_file_path)
+                    s3.download_file(BUCKET_NAME, file_name, local_file_path)
                     print(f'Downloaded {file_name} to {local_file_path}')
 
     list_files()
@@ -56,7 +59,7 @@ def load_pdfs(bucket_name, local_directory):
     for root, dirs, files in os.walk(local_directory):
         for file_name in files:
             if file_name.endswith('.pdf'):
-                print('Found a PDF')
+                # print('Found a PDF')
                 file_path = os.path.join(root, file_name)
                 """
                 Generic PyPDFLoader to load PDFs and split into documents.
@@ -66,6 +69,14 @@ def load_pdfs(bucket_name, local_directory):
                 loader = PyPDFLoader(file_path=file_path)
                 docs = loader.load_and_split(text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100))
                 print(f"loading {file_name} into pinecone index")
+                # Customizing metadata to include bucket name and source key
+                # This will allow RAG apps to retrieve the original file from S3
+                for i in range(len(docs)):
+                    dm = docs[i].metadata
+                    dm['source'] = '/'.join(dm['source'].split('/')[1:])
+                    dm['bucket'] = BUCKET_NAME
+                    docs[i] = Document(page_content=docs[i].page_content, metadata=dm)               
+
                 vector_store.add_documents(docs)
                 # now delete the local file to clean up after ourselves
                 os.remove(file_path)
@@ -73,17 +84,10 @@ def load_pdfs(bucket_name, local_directory):
 
 """
 Main function to call load_pdfs. 
-bucket_name and local_directory must be provided. Throw error if not
+Specific bucket_name in .env file
 """
-def main(bucket_name, local_directory):
-    if bucket_name is None or local_directory is None:
-        raise ValueError("bucket_name and local_directory must be provided")
-    load_pdfs(bucket_name, local_directory)
+def main():
+    load_pdfs()
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Load PDFs from a local directory to a bucket.')
-    parser.add_argument('bucket_name', help='The name of the bucket')
-    parser.add_argument('local_directory', help='The local directory to load PDFs from')
-    args = parser.parse_args()
-
-    main(args.bucket_name, args.local_directory)
+if __name__ == "__main__":    
+    main()
